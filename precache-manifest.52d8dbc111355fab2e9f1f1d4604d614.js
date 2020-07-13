9ea54d5b2bb4a5af04d5d132eed086b0f0ee9e53
Aswad Ali <aswadgemini11@gmail.com>
2020-07-13 13:43:37 +0500

    Initial commit

---
+++ precache-manifest.52d8dbc111355fab2e9f1f1d4604d614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e17627a43a27045b36a0199f9cc073",
+    "url": "/yottahostsapi/index.html"
+  },
+  {
+    "revision": "9bad3c2948e323b3effa",
+    "url": "/yottahostsapi/static/js/2.6b171773.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yottahostsapi/static/js/2.6b171773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90aae8b361ad96b7c532",
+    "url": "/yottahostsapi/static/js/main.26728a57.chunk.js"
+  },
+  {
+    "revision": "de20bb3b0bfe298ac7e7",
+    "url": "/yottahostsapi/static/js/runtime-main.75c199e4.js"
+  },
+  {
+    "revision": "1336a69b3b60ec204ed4d6685c3dfdfb",
+    "url": "/yottahostsapi/static/media/loader.1336a69b.svg"
+  }
+]);
